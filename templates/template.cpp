fc5469bb716f6524f5e56dd481bc2a30f5c46154
Andrew Dong <dongandrew99@gmail.com>
2022-01-03 12:21:00 -0500

    new trick from tourist: ifdef include

--- templates/template.cpp
+++ templates/template.cpp
@@ -14,6 +14,12 @@ Comments about problem:
 
 using namespace std;
 
+#ifdef _DEBUG
+#include "../algorithms/debug/debug.h"
+#else
+#define debug(...) 42
+#endif
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
